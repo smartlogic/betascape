f5f165783e18d508c02cd462af108362a431cbe5
Michael Berkowitz <michael.berkowitz@gmail.com>
2010-06-25 12:24:06 -0400

    Update schedule and content, fix hover states on links

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 
 $(document).ready(function(){
 						   
-	$(".day p").hide();
+	$(".day h4, .day h5, .day p").hide();
 	
 	$(".day").toggle(function() {
-		$("p", this).show(300);
+		$("h4, h5, p", this).show(300);
 		$("h3", this).css({'background-color':'#FCBA63', 'color':'#d67f2a'});
 	}, function() {
-		$("p", this).hide(300);
+		$("h4, h5, p", this).hide(300);
 		$("h3", this).css({'background-color':'#ace3fa', 'color':'#008ab3'});
 	});	
 		
